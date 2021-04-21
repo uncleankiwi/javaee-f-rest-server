c49b182f762c9c33b5f8eb5338234d15158ab906
uncleankiwi <feldtspirit@gmail.com>
2021-04-21 10:28:08 -0400

    -

--- src/main/java/com/kovunov/service/LeagueService.java
+++ src/main/java/com/kovunov/service/LeagueService.java
@@ -1,4 +1,8 @@
 package com.kovunov.service;
 
+import com.kovunov.entity.Team;
+
 public interface LeagueService {
+	void createLeague();
+	void addTeamToLeague(Team team);
 }
--- src/main/java/com/kovunov/service/TeamService.java
+++ src/main/java/com/kovunov/service/TeamService.java
@@ -1,4 +1,10 @@
 package com.kovunov.service;
 
+import com.kovunov.entity.Player;
+import com.kovunov.entity.Team;
+
 public interface TeamService {
+	void createTeam(Team team);
+	void addPlayerToTeam(Player player);
+	void updateTeamName(String name);
 }
